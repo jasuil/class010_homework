c713ec4a2c8aa23c2bc282903d41fd29ad61b813
home <jasuil@daum.net>
2020-10-15 22:51:49 +0900

    Initial commit

---
+++ src/main/java/net/class101/homework1/Homework1Application.java
@@ -0,0 +1,20 @@
+package net.class101.homework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@SpringBootApplication
+public class Homework1Application {
+
+	public static void main(String[] args) throws IOException {
+
+		SpringApplication.run(Homework1Application.class, args);
+
+	}
+
+}
